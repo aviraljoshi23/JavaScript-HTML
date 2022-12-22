9cb67de706e6cbb2e278df7439cb57dccbc8eabd
aviraljoshi23 <23aviraljoshi@gmail.com>
2022-12-22 15:16:53 +0530

    Second commit

---
+++ BasicQuestion/js02.js
@@ -0,0 +1,19 @@
+
+// function sum(a,b=10)
+// {
+//     add = a+b; // Initialization (Hoisting)
+//     console.log(add);
+// }
+// function sum(a)
+// {
+//     console.log(a);
+// }
+// sum(10,15);
+// var myfun=(a,b)=>{return "SUM OF A AND B "+(a+b)
+// };
+// console.log(myfun(15,41));
+
+
+var myfun=(a,b)=>{return "SUM OF A AND B "+(a+b)
+};
+console.log(myfun(15,41));
